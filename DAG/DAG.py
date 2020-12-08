cd9cc786963204ee2fd16a112729f2f30df344bc
Xiao Tan <xiao.tan6491@gmail.com>
2020-12-08 14:27:12 -0500

    update dag

--- DAG/DAG.py
+++ DAG/DAG.py
@@ -3,8 +3,9 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from csci_final_project.dataloader import filehandler
 from csci_final_project import cli
-from csci_final_project.optimizer import ga
-from csci_final_project.virtulization import graphPlotly
+from csci_final_project.optimizer.ga import GeneticAlgorithm
+from csci_final_project.virtulization import graph_plotly
+from csci_final_project.mockbacktest import backtest_output_producer
 
 default_args = {
     'owner': 'xt',
@@ -20,33 +21,50 @@ dag = DAG(
 )
 
 filename = 'sample.csv'
+file_path = '/Users/xiaotan/Course/2020fa-csci_final_project-tanxiao64/data/'
+
+generate_mock_backtest_output = PythonOperator(
+    task_id='generate_mock_backtest_output',
+    python_callable=backtest_output_producer.generate_fake_output,
+    op_kwargs={'signal_num': 5, 'row_num': 7000, 'filename': filename, 'file_path': file_path},
+    dag=dag,
+)
+
 check_data_exit = PythonOperator(
     task_id='check_data_exist',
     python_callable=filehandler.file_exist,
-    op_kwargs={'filename': filename},
+    op_kwargs={'filename': filename, 'file_path': file_path},
     dag=dag,
 )
 
 validate_data = PythonOperator(
     task_id='validate_data',
     python_callable=filehandler.validate,
-    op_kwargs={'filename': filename},
+    op_kwargs={'filename': filename, 'file_path': file_path},
     dag=dag,
 )
 
 run_optimization = PythonOperator(
     task_id='run_optimization',
     python_callable=cli.main,
-    op_kwargs={'optimizer': ga, 'filename': filename},
+    op_kwargs={'optimizer': GeneticAlgorithm, 'filename': filename, 'file_path': file_path},
     dag=dag,
 )
 
 output_optimized_graphs = PythonOperator(
-    task_id='output_graphs',
-    python_callable=graphPlotly.read_csv_file_and_plot,
-    op_kwargs={},
+    task_id='output_optimized_graphs',
+    python_callable=graph_plotly.read_csv_file_and_plot,
+    op_kwargs={'filename': 'best_trades.csv', 'file_path': file_path},
+    dag=dag,
+)
+
+output_original_graphs = PythonOperator(
+    task_id='output_original_graphs',
+    python_callable=graph_plotly.read_csv_file_and_plot,
+    op_kwargs={'filename': filename, 'file_path': file_path},
     dag=dag,
 )
 
 
-check_data_exit >> validate_data >> run_optimization >> output_optimized_graphs
+generate_mock_backtest_output >> check_data_exit >> validate_data >> run_optimization >> output_optimized_graphs
+validate_data >> output_original_graphs

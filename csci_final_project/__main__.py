d74fcfb4e79141c7bb3709ca7fab739e1c8f9bad
Xiao Tan <xiao.tan6491@gmail.com>
2020-12-08 14:26:18 -0500

    update main and cli

--- csci_final_project/__main__.py
+++ csci_final_project/__main__.py
@@ -1,4 +1,6 @@
-from .cli import main
+from csci_final_project.cli import main
+from csci_final_project.optimizer.ga import GeneticAlgorithm
 
 if __name__ == "__main__":
-    main()
+    file_path = '/Users/xiaotan/Course/2020fa-csci_final_project-tanxiao64/data/'
+    main(GeneticAlgorithm, 'sample.csv', file_path, target='pnl', output_filename='best_trades.csv')
--- csci_final_project/cli.py
+++ csci_final_project/cli.py
@@ -1,15 +1,16 @@
-from csci_final_project.optimizer.ga import OptimizationSetting, GeneticAlgorithm
+from csci_final_project.optimizer.setting import OptimizationSetting
+import logging
 
 
-def main(optimizer, filename, **kwargs):
+def main(optimizer, filename, file_path, target='pnl', output_filename='best_trades.csv', **kwargs):
     setting = OptimizationSetting()
-    setting.set_target('pnl')
-    setting.add_parameter('signal_1', start=0, end=1, step=0.5)
-    setting.add_parameter('signal_2', start=0, end=1, step=0.5)
+    setting.set_target(target)
+    setting.add_parameter('signal_1', start=0, end=1, step=0.1)
+    setting.add_parameter('signal_2', start=0, end=1, step=0.1)
 
-    ga = GeneticAlgorithm(filename='sample.csv', filepath='/Users/xiaotan/Course/2020fa-csci_final_project-tanxiao64/data/')
-    print(ga.run_optimization(setting))
-    ga.output_data_best_signal('best.csv')
+    op = optimizer(filename=filename, filepath=file_path)
+    logging.info(op.run_optimization(setting))
+    op.output_data_best_signal(output_filename)
 
 
 

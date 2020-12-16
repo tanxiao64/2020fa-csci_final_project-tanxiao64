6a573ac5517a3e75461c347a44328890721a640c
Xiao Tan <xiao.tan6491@gmail.com>
2020-12-15 22:15:08 -0500

    update dataloader and add salt

--- csci_final_project/dataloader/filehandler.py
+++ csci_final_project/dataloader/filehandler.py
@@ -1,30 +1,43 @@
 import pandas as pd
 import os
+from airflow.models import Variable
 
 
 def loadCSV(filename, file_path=None, **kwargs):
     if file_path is None:
-        file_path = 'data/'
+        file_path = "data/"
     trades = pd.read_csv(file_path + filename)
     return trades
 
+
 def saveCSV(data, filename, file_path=None, **kwargs):
     if file_path is None:
-        file_path = 'data/'
-    data.to_csv(file_path + filename)
+        file_path = "data/"
+    data.to_csv(file_path + filename, index=False)
 
 
-def file_exist(filename, file_path=None, **kwargs):
+def file_exist(filename, file_path=None, use_salt=False, **kwargs):
+    if use_salt:
+        filename = Variable.get("salt") + filename
+
     if file_path is None:
-        file_path = 'data/'
+        file_path = "data/"
     if not os.path.exists(file_path + filename):
         raise FileNotFoundError()
+    return True
 
+def validate(filename, file_path=None, use_salt=False, **kwargs):
+    if use_salt:
+        filename = Variable.get("salt") + filename
 
-def validate(filename, file_path=None, **kwargs):
     if file_path is None:
-        file_path = 'data/'
+        file_path = "data/"
 
     trades = pd.read_csv(file_path + filename)
-    if not ('start_date' in trades.columns and 'end_date' in trades.columns and 'pnl' in trades.columns):
-        raise Exception('Csv trade file does not have expected column')
+    if not (
+        "start_date" in trades.columns
+        and "end_date" in trades.columns
+        and "pnl" in trades.columns
+    ):
+        raise Exception("Csv trade file does not have expected column")
+    return True

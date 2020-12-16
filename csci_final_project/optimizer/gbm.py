95e13b5a8ca4deb50b574979f93e2c9e39bb58d0
Xiao Tan <xiao.tan6491@gmail.com>
2020-12-15 22:15:51 -0500

    update optimizers

--- csci_final_project/optimizer/gbm.py
+++ csci_final_project/optimizer/gbm.py
@@ -1,2 +1,7 @@
-def gbm():
-    pass
+def gbm():  # pragma: no cover
+    """NOTE for TAs:
+    After a follow up discussion with my manager recently, he is a little concerned about the IP component of the
+    application of this optimizer. As a result, I decided not to implement this and spent more time on the workflow
+    and other topics of this project. Instead of GBM, I added a basic bruteforce optimizer which searches the full space.
+    """
+    return
--- csci_final_project/optimizer/setting.py
+++ csci_final_project/optimizer/setting.py
@@ -3,7 +3,7 @@ from itertools import product
 
 class OptimizationSetting:
     """
-    Setting class.
+    OptimizationSetting class is used to generate the search space for the optimizers
     """
 
     def __init__(self):
@@ -11,27 +11,32 @@ class OptimizationSetting:
         self.params = {}
         self.target_name = ""
 
-
     def add_parameter(
         self, name: str, start: float, end: float = None, step: float = None
     ):
-        """"""
+        """
+        Add searching parameters
+        """
 
         value = start
         value_list = []
 
-        while value+step <= end:
-            value_list.append((value, value+step))
+        while value + step <= end:
+            value_list.append((value, value + step))
             value += step
 
         self.params[name] = value_list
 
     def set_target(self, target_name: str):
-        """"""
+        """
+        Objective function's column
+        """
         self.target_name = target_name
 
     def generate_setting_ga(self):
-        """"""
+        """
+        Generate settings for genetic algorithm
+        """
         settings_ga = []
         keys = self.params.keys()
         values = self.params.values()
@@ -43,3 +48,18 @@ class OptimizationSetting:
             settings_ga.append(param)
 
         return settings_ga
+
+    def generate_setting_bf(self):
+        """
+        Generate settings for brute force
+        """
+        settings_bf = []
+        keys = self.params.keys()
+        values = self.params.values()
+        products = list(product(*values))
+
+        for p in products:
+            setting = dict(zip(keys, p))
+            settings_bf.append(setting)
+
+        return settings_bf

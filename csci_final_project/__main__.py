0b2bd2a751b327a9745fa0a611f8fd9cedf73ea3
Xiao Tan <xiao.tan6491@gmail.com>
2020-12-15 22:16:26 -0500

    update main

--- csci_final_project/__main__.py
+++ csci_final_project/__main__.py
@@ -1,6 +1,12 @@
 from csci_final_project.cli import main
 from csci_final_project.optimizer.ga import GeneticAlgorithm
 
-if __name__ == "__main__":
-    file_path = '/Users/xiaotan/Course/2020fa-csci_final_project-tanxiao64/data/'
-    main(GeneticAlgorithm, 'sample.csv', file_path, target='pnl', output_filename='best_trades.csv')
+if __name__ == "__main__":  # pragma: no cover
+    file_path = "../data/"
+    main(
+        GeneticAlgorithm,
+        "sample.csv",
+        file_path,
+        target="pnl",
+        output_filename="best_trades.csv",
+    )
--- csci_final_project/cli.py
+++ csci_final_project/cli.py
@@ -1,16 +1,27 @@
 from csci_final_project.optimizer.setting import OptimizationSetting
 import logging
+from airflow.models import Variable
 
 
-def main(optimizer, filename, file_path, target='pnl', output_filename='best_trades.csv', **kwargs):
+def main(
+    optimizer,
+    filename,
+    file_path,
+    target="pnl",
+    output_filename="best_trades.csv",
+    use_salt=False,
+):  # pragma: no cover
+    """
+    Create settings and run optimization based on the pass in optimizer
+    """
     setting = OptimizationSetting()
     setting.set_target(target)
-    setting.add_parameter('signal_1', start=0, end=1, step=0.1)
-    setting.add_parameter('signal_2', start=0, end=1, step=0.1)
+    setting.add_parameter("signal_1", start=0, end=1, step=0.1)
+    setting.add_parameter("signal_2", start=0, end=1, step=0.1)
 
+    if use_salt:
+        filename = Variable.get("salt") + filename
+        output_filename = Variable.get("salt") + output_filename
     op = optimizer(filename=filename, filepath=file_path)
     logging.info(op.run_optimization(setting))
     op.output_data_best_signal(output_filename)
-
-
-
